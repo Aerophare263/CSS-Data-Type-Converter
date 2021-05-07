e4c3dfae34fcbf13156d74e605a17e9a66d98136
Tomasz Lisowski <t.p.lisowski@gmail.com>
2021-05-07 13:24:01 +0200

    Minor cleanup

--- js/app.js
+++ js/app.js
@@ -1,21 +1,21 @@
-function rgba2hex () {
+function rgba2hex() {
     const rgba = document.getElementById("rgbaValue").value;
     // Match color to groups with RegExp
     rgbaArray = rgba.match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
     const output = (rgbaArray && rgbaArray.length === 4) ? "#" +
-    ("0" + parseInt(rgbaArray[1],10).toString(16)).slice(-2) + // Red
-    ("0" + parseInt(rgbaArray[2],10).toString(16)).slice(-2) + //Green
-    ("0" + parseInt(rgbaArray[3],10).toString(16)).slice(-2) : ''; //Blue
+        ("0" + parseInt(rgbaArray[1], 10).toString(16)).slice(-2) + // Red
+        ("0" + parseInt(rgbaArray[2], 10).toString(16)).slice(-2) + //Green
+        ("0" + parseInt(rgbaArray[3], 10).toString(16)).slice(-2) : ''; //Blue
     //Send Output
     let hexElement = document.getElementById("hexValue");
     hexElement.value = output;
 
     // Keep text readable
     if (rgbaArray != null) {
-        if (parseInt(rgbaArray[1],10) + parseInt(rgbaArray[2],10) + parseInt(rgbaArray[3],10) >= 300) {
+        if (parseInt(rgbaArray[1], 10) + parseInt(rgbaArray[2], 10) + parseInt(rgbaArray[3], 10) >= 300) {
             document.body.style.color = "#000000";
         }
-        else if (parseInt(rgbaArray[1],10) >= 200 || parseInt(rgbaArray[2],10) >= 200 || parseInt(rgbaArray[3],10) >= 200) {
+        else if (parseInt(rgbaArray[1], 10) >= 200 || parseInt(rgbaArray[2], 10) >= 200 || parseInt(rgbaArray[3], 10) >= 200) {
             document.body.style.color = "#000000";
         }
         else {
@@ -29,6 +29,6 @@ function rgba2hex () {
     document.body.style.backgroundColor = rgba;
 }
 
-function hex2rgba () {
+function hex2rgba() {
     console.log("hex2rgba");
 }
